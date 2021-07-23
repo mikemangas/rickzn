fc03d6ab630c7f3adaeeae372f607ef33503d546
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-23 20:37:19 +0200

    .

--- src/comp/Header.js
+++ src/comp/Header.js
@@ -4,7 +4,9 @@ import logo from "../img/rickmortylogo.png";
 export default function Header() {
   return (
     <header className="App__header">
-      <img class="App__header__logo" src={logo} alt="rickNmorty-logo"></img>
+      <div className="App__header--left">
+        <img class="App__header__logo" src={logo} alt="rickNmorty-logo"></img>
+      </div>
       <div className="App__header__navLinks">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/characters">Characters</NavLink>
