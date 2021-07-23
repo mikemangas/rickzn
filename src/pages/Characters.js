ab352a45a51a49a6eea56b1e194e6b94f36775a5
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-23 13:13:59 +0200

    add fetch functionality to the characters page

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,1 +1,31 @@
-export default function Characters() {}
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function Characters() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const url = "https://rickandmortyapi.com/api/character";
+    fetch(url)
+      .then((res) => res.json())
+      .then((characterData) => {
+        setCharacters(characterData.results);
+      });
+  }, []);
+
+  function renderCharacters() {
+    return characters.map((character) => {
+      const id = character?.id;
+      return (
+        <div key={id} className="characters-wrapper">
+          <Link to={`/characters/${id}`}>
+            <h2>{character?.name}</h2>
+            <img src={character?.image} alt={character?.name}></img>
+          </Link>
+        </div>
+      );
+    });
+  }
+
+  return renderCharacters();
+}
