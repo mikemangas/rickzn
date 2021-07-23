6045aeb2bf9d644c47c4d9fa02b28132c05ad082
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-23 12:09:57 +0200

    add main into appjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Main from "./Components/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
