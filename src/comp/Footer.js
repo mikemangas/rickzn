f7fd8a671fc4de9cef7ed14b998c01d4dd10ebd7
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-23 21:10:37 +0200

    add styling to charcters in characterlist page

--- src/comp/Footer.js
+++ src/comp/Footer.js
@@ -1,7 +1,13 @@
 export default function Footer() {
   return (
     <footer className="App__footer">
-      <p>Made with love by Mike</p>
+      <div className="App__footer--love">
+        <p>Made with love by Mike</p>
+      </div>
+      <div className="App__footer--contact">
+        <a href="tel:+49176567360">Give me a call</a>
+        <a href="mailto:mangasarov@hotmail.de">Send me a Mail</a>
+      </div>
     </footer>
   );
 }
