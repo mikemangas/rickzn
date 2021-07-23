7592ae87cf8c44371ae8d32beebc76a644259c3c
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-23 11:37:23 +0200

    add home.js and basic informations for the home page, with img

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import rickhome from "../img/RickMorty.webp";
+
+export default function Home() {
+  return (
+    <main>
+      <h2>Welcome to the Rick and Morty app</h2>
+      <img src={rickhome} alt="rickHome-img"></img>
+    </main>
+  );
+}
