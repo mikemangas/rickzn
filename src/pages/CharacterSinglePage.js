0120c48eb38befe86bd667ad7a6aa3b6c8345370
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-23 14:29:57 +0200

    add charactersinglepage

--- src/pages/CharacterSinglePage.js
+++ src/pages/CharacterSinglePage.js
@@ -1,1 +1,27 @@
-export default function CharacterSinglePage() {}
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function CharacterSinglePage() {
+  const characterId = useParams();
+  const [character, setCharacter] = useState();
+
+  useEffect(() => {
+    const url = `https://rickandmortyapi.com/api/character/${characterId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((characterData) => {
+        setCharacter(characterData);
+      });
+  }, [characterId]);
+
+  //   function renderCharacters() {
+  //     return (
+  //       <div className="character-single-page">
+  //         <h2>{character?.name}</h2>
+  //         <img src={character?.image} alt={character?.name}></img>
+  //       </div>
+  //     );
+  //   }
+  //   return renderCharacters();
+  return console.log(character.results);
+}
