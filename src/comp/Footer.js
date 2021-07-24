c9a08b63b12f06233836998d072691aacf4d178e
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-24 23:22:44 +0200

    modified footer (made it sticky and added 2 imgs, improved some additional styling, like h2 in characters page)

--- src/comp/Footer.js
+++ src/comp/Footer.js
@@ -1,3 +1,6 @@
+import phone from "../img/phone-call.png";
+import email from "../img/email.png";
+
 export default function Footer() {
   return (
     <footer className="App__footer">
@@ -5,8 +8,15 @@ export default function Footer() {
         <p>Made with love by Mike</p>
       </div>
       <div className="App__footer--contact">
-        <a href="tel:+49176567360">Give me a call</a>
-        <a href="mailto:mangasarov@hotmail.de">Send me a Mail</a>
+        <a className="App__footer--contact--phone" href="tel:+49176567360">
+          <img src={phone} alt="phone-call"></img>
+        </a>
+        <a
+          className="App__footer--contact--email"
+          href="mailto:mangasarov@hotmail.de"
+        >
+          <img src={email} alt="email"></img>
+        </a>
       </div>
     </footer>
   );
