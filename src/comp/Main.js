d54b3626a6133fdc6af97181578dcf75a73a1cb3
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-24 13:11:25 +0200

    add error 404 page

--- src/comp/Main.js
+++ src/comp/Main.js
@@ -2,18 +2,22 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Characters from "../pages/Characters";
 import CharacterSinglePage from "../pages/CharacterSinglePage";
+import NoMatch from "../pages/NoMatch";
 export default function Main() {
   return (
     <main className="App__main">
       <Switch>
-        <Route path="/characters/:characterId">
+        <Route exact path="/characters/:characterId">
           <CharacterSinglePage />
         </Route>
-        <Route path="/characters">
+        <Route exact path="/characters">
           <Characters />
         </Route>
-        <Route path="/">
+        <Route exact path="/">
           <Home />
+        </Route>
+        <Route path="/">
+          <NoMatch />
         </Route>
       </Switch>
     </main>
